860cd3ac91b87e13e0f9241b54a2834f2ca31447
unknown <bolajijohnson19@gmail.com>
2022-08-07 01:23:52 +0100

    Checkout page,item and cart total

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,37 @@
-import './cart-dropdown.styles.scss'
+import { useContext } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
-    <div className='cart-dropdown-container'>
-        <div className='cart-items' />
-        <Button>GO TO CHECKOUT</Button>
-    </div>
-);
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () =>
+{
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const goToCheckoutHandler = () =>
+    {
+        navigate('/checkout')
+    }
+
+    return (
+        <div className='cart-dropdown-container'>
+            <div className='cart-items'>
+                {cartItems.length ? (
+                    cartItems.map((cartItem) => (
+                        <CartItem key={cartItem.id} cartItem={cartItem} />
+                    ))
+                ) : (
+                    <span className='empty-message'>Your cart is empty</span>
+                )}
+            </div>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+        </div>
+    );
+};
 
 export default CartDropdown;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,15 +7,17 @@ import './cart-icon.styles.scss'
 
 const CartIcon = () =>
 {
-    const { isCartOpen, setIsCartOpen } = useContext(CartContext)
+    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext)
 
     const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen)
+
+    // const 
 
 
     return (
         <div className='cart-icon-container'>
             <ShoppingIcon className='shopping-icon' onClick={toogleIsCartOpen} />
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartCount}</span>
         </div>)
 }
 
